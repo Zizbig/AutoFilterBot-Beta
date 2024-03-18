0e6e4e2981f6a7720363022aff5f23d7e15f017d
Zizbig <69723388+Zizbig@users.noreply.github.com>
2024-03-18 20:56:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -31,19 +31,18 @@ class temp(object):
     GROUPS_CANCEL = False
     BOT = None
 
-async def is_subscribed(bot, query, channel):
-    btn = []
-    for id in channel:
-        chat = await bot.get_chat(int(id))
-        try:
-            await bot.get_chat_member(id, query.from_user.id)
-        except UserNotParticipant:
-            btn.append(
-                [InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)]
-            )
-        except Exception as e:
-            pass
-    return btn
+async def is_subscribed(bot, query):
+    try:
+        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+    except UserNotParticipant:
+        pass
+    except Exception as e:
+        logger.exception(e)
+    else:
+        if user.status != enums.ChatMemberStatus.BANNED:
+            return True
+
+    return False
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
